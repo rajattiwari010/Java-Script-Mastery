4c2a5324a400ee6dc69d6f138b7e379fc118ba87
Rajat Tiwari <rajattiwari006@gmail.com>
2023-03-12 08:35:12 +0530

    Add files via upload

---
+++ javascript/parameters/script8.js
@@ -0,0 +1,8 @@
+console.log(`connected`);
+//parameters-used when defining a function
+//arguments- real value passed when function called
+
+const greet=(name,time)=>{
+    console.log(`hi ${name} whats the ${time}`);
+}
+greet(`Rajat`,`6pm`);
